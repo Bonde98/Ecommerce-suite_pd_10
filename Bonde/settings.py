a51f701d022f523daadb52a140599d787faa01c6
Bonde98 <badianeasse99@gmail.com>
2022-05-16 14:32:22 +0200

    modifier

--- Bonde/settings.py
+++ Bonde/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 #STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
-django_heroku.settuings(locals())
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
